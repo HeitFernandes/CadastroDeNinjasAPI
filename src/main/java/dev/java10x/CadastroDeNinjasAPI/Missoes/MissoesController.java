5e0064645549c47ba6ea97b68325106685f034d1
Heitor <fernandesheitort2@gmail.com>
2025-08-18 11:34:57 -0300

    feat: Adiciona @anotations para o controller de Missoes

--- src/main/java/dev/java10x/CadastroDeNinjasAPI/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/Missoes/MissoesController.java
@@ -1,4 +1,10 @@
 package dev.java10x.CadastroDeNinjasAPI.Missoes;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+
 }
