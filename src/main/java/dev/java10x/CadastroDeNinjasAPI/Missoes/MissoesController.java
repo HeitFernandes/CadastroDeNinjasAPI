50cfd6734decb3d3aaa473f1df964afa13576156
Heitor <fernandesheitort2@gmail.com>
2025-08-19 13:53:19 -0300

    feat: Camada de Service feita e integrada com o controller de missoes

--- src/main/java/dev/java10x/CadastroDeNinjasAPI/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/Missoes/MissoesController.java
@@ -2,20 +2,33 @@ package dev.java10x.CadastroDeNinjasAPI.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
 
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     // GET - MANDAR UMA REQUISICAO PARA MOSTRAR AS MISSOES
     @GetMapping("/listar")
-    public String listarMissao(){
-        return "missoes listadas com sucesso";
+    public List<MissoesModel> listarMissao(){
+        return missoesService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissoesPorId(@PathVariable Long id){
+        return missoesService.listarMissoesPorID(id);
     }
 
     // POST - MANDAR UMA REQUISiCAO PARA CRIAR AS MISSOES
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "missao criada com sucesso";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missoes){
+        return missoesService.criarMissao(missoes);
     }
 
     // PUT - Mudar a missao, ou algum dado
@@ -25,10 +38,11 @@ public class MissoesController {
     }
 
     // Delete para apagar missoes
-    @DeleteMapping("/alterar")
-    public String deletarMissao(){
-        return "missao deletada com sucesso";
+    @DeleteMapping("/deletarID/{id}")
+    public void deletarMissaoPorId(@PathVariable Long id){
+        missoesService.deletarMissaoPorID(id);
     }
+
 
 
 
--- src/main/java/dev/java10x/CadastroDeNinjasAPI/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/Missoes/MissoesService.java
@@ -1,4 +1,42 @@
 package dev.java10x.CadastroDeNinjasAPI.Missoes;
 
+
+import dev.java10x.CadastroDeNinjasAPI.Ninjas.NinjaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Criar missao
+    public MissoesModel criarMissao(MissoesModel missoes){
+        return missoesRepository.save(missoes);
+    }
+
+    // Listar todas as missoes
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    //listar missoes por id
+    public MissoesModel listarMissoesPorID(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
+    }
+
+    //Deletar missoes
+    public void deletarMissaoPorID(Long id){
+        missoesRepository.deleteById(id);
+    }
+
+
+
 }
