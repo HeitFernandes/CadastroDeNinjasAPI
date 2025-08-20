1ae275d8459c759230728ba99289e0c10e70c802
Heitor <fernandesheitort2@gmail.com>
2025-08-20 16:19:45 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/dev/java10x/CadastroDeNinjasAPI/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/Ninjas/NinjaService.java
@@ -9,9 +9,11 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     //Listar todos os meus ninjas
@@ -26,8 +28,10 @@ public class NinjaService {
     }
 
     //Criar um novo ninja
-    public NinjaModel criarNinja(NinjaModel ninja) {
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO) {
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     // Deletar o ninja - Tem que ser o metodo void
@@ -35,6 +39,7 @@ public class NinjaService {
         ninjaRepository.deleteById(id);
     }
 
+    //Atualizar ninja
     public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado) {
         if (ninjaRepository.existsById(id)) {
             ninjaAtualizado.setId(id);
