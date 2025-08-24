4b1ffc10ebb2c1e570b3d2cf98f2b85082aef6d6
Heitor <fernandesheitort2@gmail.com>
2025-08-24 16:25:42 -0300

    feat: Tira a responsabilidade do meu Model e pssa para dentro do meu DTO, usando streams no mapper

--- src/main/java/dev/java10x/CadastroDeNinjasAPI/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjasAPI/Ninjas/NinjaController.java
@@ -28,19 +28,19 @@ public class NinjaController {
 
     // Mostrar todos os ninjas (READ)
     @GetMapping("/listar")
-    public List<NinjaModel> listarNinjas(){
+    public List<NinjaDTO> listarNinjas(){
         return ninjaService.listarNinjas();
     }
 
     // mostrar ninjas Por Id (READ)
     @GetMapping("listar/{id}")
-    public NinjaModel listarNinjasPorID(@PathVariable Long id){
+    public NinjaDTO listarNinjasPorID(@PathVariable Long id){
         return ninjaService.listarNinjasPorID(id);
     }
 
     // alterar dados dos ninjas por ID(UPDATE)
     @PutMapping("/atualizar/{id}")
-    public NinjaModel alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado){
+    public NinjaDTO alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado){
         return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
